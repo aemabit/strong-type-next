05d5a8746f540998eaf1d98d1a9a7d26b3760926
Andres Mejias <aemabit@gmail.com>
2021-02-05 15:10:53 -0500

    render app with material ui

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,26 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core";
+import React from "react";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    // RENDER APP AND GET CTX OF THE COLLECTED SIDE EFFECTS
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    return {
+      ...initialProps,
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    };
   }
 
   render() {
