9f567dd043640e8b7e03d56acf5fe9908a7aee37
Andres Mejias <aemabit@gmail.com>
2021-02-09 22:42:29 -0500

    header integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apollo";
 import { themeDark, themeLight } from "lib/theme";
+import { AuthProvider } from "lib/useAuth";
+import Header from "components/Header";
 
 export default function MyApp({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
@@ -25,7 +27,10 @@ export default function MyApp({ Component, pageProps }) {
     <ApolloProvider client={apolloClient}>
       <ThemeProvider theme={darkState ? themeDark : themeLight}>
         <CssBaseline />
-        <Component {...pageProps} />
+        <AuthProvider>
+          <Header darkState={darkState} handleThemeChange={handleThemeChange} />
+          <Component {...pageProps} />
+        </AuthProvider>
       </ThemeProvider>
     </ApolloProvider>
   );
